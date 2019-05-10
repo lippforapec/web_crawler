e67a36f1fae6d36390a9305e6977213877ea1ccf
hyerim1048 <gpflav@gmail.com>
2019-05-10 23:27:38 +0900

    update

--- google/ScrapGoogle.py
+++ google/ScrapGoogle.py
@@ -1,75 +1,153 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+import psycopg2
+import os
+import categories 
+import itertools
+import json
+from datetime import datetime
 
 USER_AGENT = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
+class GoogleScraper:
+    def __init__(self):
+        self.dbname = "lipp"
+        self.tablename = "app_search"
+        self.results = []
+        self.categories = categories.CATEGORIES
+        self.topics = categories.TOPICS
+        self.queries = []
+        self.news_topic = categories.NEWS_TOPICS
 
-def fetch_results(search_term, number_results, language_code):
-    assert isinstance(search_term, str), 'Search term must be a string'
-    assert isinstance(number_results, int), 'Number of results must be an integer'
-    escaped_search_term = search_term.replace(' ', '+')
+    def fetch_results(self,search_term, number_results, language_code):
+        assert isinstance(search_term, str), 'Search term must be a string'
+        assert isinstance(number_results, int), 'Number of results must be an integer'
+        escaped_search_term = search_term.replace(' ', '+')
 
-    google_url = 'https://www.google.com/search?q={}&num={}&hl={}'.format(escaped_search_term, number_results, language_code)
-    response = requests.get(google_url, headers=USER_AGENT)
-    response.raise_for_status()
-    return search_term, response.text
-
-
-def parse_results(html, keyword):
-    soup = BeautifulSoup(html, 'html.parser')
-    # html parsing 구조가 바뀜
-    found_results = []
-    rank = 1
-    result_block = soup.find_all('div', attrs={'class': 'g'})
-    print(len(result_block))
-    for result in result_block:
-        try: 
-            class_r = result.find('div', attrs={'class': 'r'})
-            link = class_r.find('a', href=True)
-            title = class_r.find('h3')
-            description = result.find('span', attrs={'class': 'st'})
-            if link and title:
-                link = link['href']
-                title = title.get_text()
-                if description:
-                    description = description.get_text()
-                if link != '#':
-                    found_results.append({'keyword': keyword, 'rank': rank, 'title': title, 'description': description, 'link': link})
-                    rank += 1
-        except Exception as e:
-            print(e)
-    return found_results
+        google_url = 'https://www.google.com/search?q={}&num={}&hl={}'.format(escaped_search_term, number_results, language_code)
+        response = requests.get(google_url, headers = USER_AGENT)
+        response.raise_for_status()
+        return search_term, response.text
 
 
-def scrape_google(search_term, number_results, language_code):
-    try:
-        keyword, html = fetch_results(search_term, number_results, language_code)
-        results = parse_results(html, keyword)
-        return results
-    except AssertionError:
-        raise Exception("Incorrect arguments parsed to function")
-    except requests.HTTPError:
-        raise Exception("You appear to have been blocked by Google")
-    except requests.RequestException:
-        raise Exception("Appears to be an issue with your connection")
+    def fetch_news_results(self,search_term, number_results, language_code):
+        assert isinstance(search_term, str), 'Search term must be a string'
+        assert isinstance(number_results, int), 'Number of results must be an integer'
+        escaped_search_term = search_term.replace(' ', '+')
+
+        google_url = 'https://www.google.com/search?q={}&num={}&hl={}&tbm=nws'.format(escaped_search_term, number_results, language_code)
+        response = requests.get(google_url, headers = USER_AGENT)
+        response.raise_for_status()
+        return search_term, response.text
+
+    # PARSING  다르게 해야함
+    def parse_results(self,html, query):
+        soup = BeautifulSoup(html, 'html.parser')
+        rank = 1
+        found_results =[]
+        result_block = soup.find_all('div', attrs={'class': 'g'})
+        #print(result_block)
+        for result in result_block:
+            try: 
+                link = result.find('a', href=True)
+                #print(link)
+                title = result.find('h3')
+                description = result.find('div', attrs={'class': 'st'})
+                #print(link,title,description)
+                if link and title:
+                    link = link['href']
+                    title = title.get_text()
+                    if description:
+                        description = description.get_text()
+                    if link != '#':
+                        found_results.append({'rank': rank, 'title': title, 'summary': description, 'link': link})
+                        rank += 1
+            except Exception as e:
+                print(e)
+        if len(found_results) >0:
+            print(len(found_results))
+            return json.dumps(found_results)
+        else:
+            return
 
 
-#if __name__ == '__main__':
-def get_keywords(keyword_args_list):
-    # TODO : keywords 에 우리가 정의한 키워드들 problem, solution 붙이기 
-    keywords = keyword_args_list
-    data = []
-    for keyword in keywords:
+    def scrape_google(self,search_term, number_results, language_code,news):
         try:
-            results = scrape_google(keyword, 5, "en")
-            print(results)
-            for result in results:
-                data.append(result)
-        except Exception as e:
-            print(e)
-        #finally:
-            #time.sleep(10)
-    return data
+            print(search_term)
+            query = ""
+            html = ""
+            if news:
+                query, html = self.fetch_news_results(search_term, number_results, language_code)
+                results = self.parse_results(html, query)
+                return results
+            else: 
+                query, html = self.fetch_results(search_term, number_results, language_code)
+                results = self.parse_results(html, query)
+                return results
+        except AssertionError:
+            raise Exception("Incorrect arguments parsed to function")
+        except requests.HTTPError:
+            raise Exception("You appear to have been blocked by Google")
+        except requests.RequestException:
+            raise Exception("Appears to be an issue with your connection")
+
+
+
+    def get_results(self):
+        self.results = [(self.scrape_google(category[1]+" "+topic[1],10,"en",0)
+                        , category[1]+" "+topic[1], category[0], topic[0],datetime.now()) 
+                        for category, topic 
+                        in list(itertools.product(self.categories,self.topics))]
+        
+        print(self.results)
+        print(tuple(self.results))
+        return 
+
+    def get_news_result(self):
+        #print(list(itertools.product(self.categories,self.news_topic)))
+        self.results = [(self.scrape_google(category[1]+" "+topic[1],10,"en",1)
+                        , category[1]+" "+topic[1], category[0], topic[0],datetime.now()) 
+                        for category, topic 
+                        in list(itertools.product(self.categories,self.news_topic))]
+        
+        print(self.results[:10])
+        print(tuple(self.results))
+        return 
+
+    def db_connect_test(self):
+        try:
+            connection = psycopg2.connect(user = "dbmasteruser",
+                                        password = "test123123!!",
+                                        host = "ls-7e30d342141246f3356ad380e9f1c23023ed3292.cswx9udslx8o.us-west-2.rds.amazonaws.com",
+                                        port = "5432",
+                                        database = "lipp")
+
+            cursor = connection.cursor()
+            # Print PostgreSQL Connection properties
+            sql = """
+                    INSERT INTO app_search(results,query, category,topic,created_at)
+                    VALUES (%s,%s,%s,%s,%s)
+                """#.format(self.tablename)
+            cursor.executemany(sql, tuple(self.results))
+            #print ( connection.get_dsn_parameters(),"\n")
+            # Print PostgreSQL version
+            cursor.execute("SELECT * from public.app_search")
+            record = cursor.fetchone()
+            print("You are connected to - ", record,"\n")
+        except (Exception, psycopg2.Error) as error :
+            print ("Error while connecting to PostgreSQL", error)
+
+    #def result_to_table(self):
+
+
+if __name__ == '__main__':
+    test=GoogleScraper()
+    test.get_news_result()
+    test.db_connect_test()
+
+    
+
+    
 
 
---
+++ google/categories.py
@@ -0,0 +1,59 @@
+CATEGORIES = (
+        (0, 'Administrative Services'),
+        (1, 'Advertising'),
+        (2, 'Agriculture and Farming'),
+        (3, 'Apps'),
+        (4, 'Artificial Intelligence'),
+        (5, 'Biotechnology'),
+        (6, 'Clothing and Apparel'),
+        (7, 'Commerce and Shopping'),
+        (9, 'Community and Lifestyle'),
+        (10, 'Consumer Electronics'),
+        (11, 'Consumer Goods'),
+        (12, 'Content and Publishing'),
+        (13, 'Data and Analytics'),
+        (14, 'Design'),
+        (15, 'Education'),
+        (16, 'Energy'),
+        (17, 'Events'),
+        (18, 'Financial Services'),
+        (19, 'Food and Beverage'),
+        (20, 'Gaming'),
+        (21, 'Government and Military'),
+        (22, 'Hardware'),
+        (23, 'Health Care'),
+        (24, 'Information Technology'),
+        (25, 'Internet Services'),
+        (26, 'Lending and Investments'),
+        (27, 'Manufacturing'),
+        (28, 'Media and Entertainment'),
+        (29, 'Messaging and Telecommunications'),
+        (30, 'Music and Audio'),
+        (31, 'Mobile'),
+        (32, 'Natural Resources'),
+        (33, 'Navigation and Mapping'),
+        (34, 'Payments'),
+        (35, 'Platforms'),
+        (36, 'Privacy and Security'),
+        (37, 'Professional Services'),
+        (38, 'Real Estate'),
+        (39, 'Sales and Marketing'),
+        (40, 'Science and Engineering'),
+        (41, 'Software'),
+        (42, 'Sports'),
+        (43, 'Sustainability'),
+        (44, 'Transportation'),
+        (45, 'Travel and Tourism'),
+        (46, 'Video')
+    )
+
+TOPICS = (
+    ('background','market problem')
+    ,('market','market size')
+    ,('solution','market solution')
+    ,('business_model','market business models')
+    ,('future','market future')
+    , ('investment','market investment')
+)
+
+NEWS_TOPICS = (('news','market investment'),)
